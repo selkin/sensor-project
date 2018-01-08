40302be8bedc44e98ae8829707f1cd244b62c9ff
SEF <selkinfr@gmail.com>
2018-01-08 02:36:00 +0000

    Made it to Ch5 and the code is broken

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const getCachedSensorReadings = require('./get-cached-sensor-readings')
 
-app.get('/temperature', function(req, res) {
-  res.send('24 °C');
-});
+/*
+We now utilize the synchronous methods exported from the 'get-cached-sensor-readings' module
+*/
+app.get('/temperature', function (req, res) {
+  res.send(getCachedSensorReadings.getTemperature().toFixed(1) + '°C')
+})
 
-app.get('/humidity', function(req, res) {
- res.send('48%');
-});
+app.get('/humidity', function (req, res) {
+  res.send(getCachedSensorReadings.getHumidity().toFixed(1) + '%')
+})
 
-app.listen(3000, function(){
- console.log('Server listening on port 3000');
-});
+app.listen(3000, function () {
+  console.log('Server listening on port 3000')
+})
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const sensor = require('node-dht-sensor'); 
+
+app.get('/temperature', function (req, res) {
+  sensor.read(11, 4, function (err, temperature, humidity) {
+	if (!err) {
+	  res.send(temperature.toFixed(1) + '°C');
+	}
+  });
+});
+
+app.get('/humidity', function (req, res) {
+  sensor.read(11, 4, function (err, temperature, humidity) {
+	if (!err) {
+	  res.send(humidity.toFixed(1) + '%');
+	}
+  });
+});
+
+app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
+
+const express = require('express');
+const app = express();
+const sensor = require('node-dht-sensor'); 
+
+app.get('/temperature', function (req, res) {
+  sensor.read(11, 4, function (err, temperature, humidity) {
+	if (!err) {
+	  res.send(temperature.toFixed(1) + '°C');
+	}
+  });
+});
+
+app.get('/humidity', function (req, res) {
+  sensor.read(11, 4, function (err, temperature, humidity) {
+	if (!err) {
+	  res.send(humidity.toFixed(1) + '%');
+	}
+  });
+});
+
+app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
